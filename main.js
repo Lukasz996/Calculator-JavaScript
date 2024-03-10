7c5852347c434b658d5030f021d1a2ad6274b9a4
Łukasz <lukaszkogut1996@gmail.com>
2024-03-10 19:20:41 +0100

    styling the calculator

---
+++ main.js
@@ -0,0 +1,14 @@
+const previousNumber = document.querySelector('previousNumber p')
+const matchSign = document.querySelector('matchSign')
+const currentNumber = document.querySelector('currentNumber')
+const clearButton = document.querySelector('clear')
+const operatorsButtons = document.querySelector('operator')
+const numbersButtons = document.querySelectorAll('number')
+const equalsButton = document.querySelector('equals')
+const calculatorHistory = document.querySelector('caulculator-history')
+const historyBtn = document.querySelector('history-btn')
+const historyItem = document.querySelectorAll('history-item')
+
+let result = ''
+
+operatorsButtons.forEach(button) => button.addEventListener
